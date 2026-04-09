b9a78af5ec5399b949b6d6dcfc0767ffa9dcedae
diawsz67 <sneine.pepe666@atomicmail.io>
2026-04-09 20:28:55 +0300

    :0

--- grafic_statistic.py
+++ grafic_statistic.py
@@ -1,60 +1,59 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from log import log
+from log import log #импортируем декоратор
 
 
 class AAPL:
     def __init__(self, file_path, start_date='2016-12-12', end_date='2022-01-09'):
         self.df = pd.read_csv(file_path)
-        self.df['Date'] = pd.to_datetime(self.df['Date'])
-        self.df = self.df[(self.df['Date'] >= start_date) & (self.df['Date'] <= end_date)]
-        self.df = self.df.reset_index(drop=True)
+        self.df['Date'] = pd.to_datetime(self.df['Date'])   # переводим строки с датами в datetime
+        self.df = self.df[(self.df['Date'] >= start_date) & (self.df['Date'] <= end_date)]   # берем только нужный промежуток времени
+        self.df = self.df.reset_index(drop=True)   #нумеруем строки по порядку
     
     
     
-    #https://www.tutorialspoint.com/article/how-to-create-a-candlestick-chart-in-matplotlib
+    #https://www.tutorialspoint.com/article/how-to-create-a-candlestick-chart-in-matplotlib построение графика японских свечей
     @log
     def plot_stock_prices(self):
-        plt.figure(figsize=(10,5), dpi = 80,  facecolor="#2d3bd6")
+        plt.figure(figsize=(10,5), dpi = 80,  facecolor="#2d3bd6")   #создаем график с синим фоном
         ax = plt.gca()
         ax.set_facecolor("#0b3675")
 
-
+        # делим дни на те, где цена выросла и где упала
         up = self.df[self.df.Close >= self.df.Open]
         down = self.df[self.df.Close < self.df.Open]
         
-        # Тела свечей
+        # тела свечей
         plt.bar(up.index, up.Close - up.Open, 0.5, bottom=up.Open, color="#2ae78b")
         plt.bar(down.index, down.Close - down.Open, 0.5, bottom=down.Open, color="#b81334")
         
-        # Тени (фитили)
+        # Тени, показвают максимальную и минимальную цену
         plt.bar(up.index, up.High - up.Close, 0.08, bottom=up.Close, color='white')
         plt.bar(up.index, up.Low - up.Open, 0.08, bottom=up.Open, color='white')
         plt.bar(down.index, down.High - down.Open, 0.08, bottom=down.Open, color='white')
         plt.bar(down.index, down.Low - down.Close, 0.08, bottom=down.Close, color='white')
         
+
+        #подписываем заголовок и оси
         plt.title('СТОИМОСТЬ АКЦИЙ APPLE', fontsize = 12, fontweight = 'bold', color='white')
         plt.xlabel('Дата', color='white')
         plt.ylabel('Цена', color='white')
         plt.grid(True, alpha = 0.3, color='white')
 
         
-
+        # отступы по краям графика, чтобы свечки по краям не обрезались
         max_index = len(self.df) - 1
         plt.xlim(-0.5, max_index + 0.5)
 
+        # показываем даты на оси X (5 штук, чтобы подписи не налезали друг на друга)
         step = max(1, len(self.df) // 5)
         plt.xticks(
                 ticks = range(0, len(self.df), step), 
                 labels = self.df['Date'].iloc[::step].dt.strftime('%d.%m.%Y'),
-                rotation=45,
+                rotation=45, # поворачиваем подписи, чтоб было читаемо
                 color='white'
         )
         plt.tick_params(colors='white')
         plt.tight_layout()
         plt.show()
 
-
-
-if __name__ == '__main__':
-    main()
